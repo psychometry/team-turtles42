3f99154e736b3d39af56c4f2c59ff6bacf6ce905
Yosevu Kilonzo <yosevuk@gmail.com>
2017-07-26 22:04:39 -0500

    Eject create-react-app - Add node-sass and sass-loader

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App 
+        Hello
+        <div className="">Hi</div> 
       </div>
     );
   }
