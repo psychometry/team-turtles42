64fa4f95ba1c317002e0805e6e080bfd7bb68f77
Yosevu Kilonzo <yosevuk@gmail.com>
2017-07-28 10:33:05 -0500

    Add unsplash and load random background - Make background colors of components transparent

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,34 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import './App.scss';
+import Unsplash, { toJson } from 'unsplash-js';
 
+const unsplash = new Unsplash({
+  applicationId: process.env.REACT_APP_UNSPLASH_APP_ID,
+  secret: process.env.REACT_APP_UNSPLASH_APP_SECRET,
+  callbackUrl: process.env.REACT_APP_UNSPLASH_CALLBACK_URL
+});
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      backgroundURL: null
+    }
+  }
+  componentDidMount() {
+    unsplash.photos.getRandomPhoto(/*{ filter photos }*/)
+      .then(toJson)
+      .then(json => {
+        this.setState({
+          backgroundURL: json.urls.regular
+        });
+      });
+  }
   render() {
+    const { backgroundURL } = this.state;
+
     return (
-      <div className="App">
+      <div className="App" style={{ backgroundImage: `url(${backgroundURL})` }}>
         <Header />
         <Main />
         <Footer />
