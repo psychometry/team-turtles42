6441cf9996ad61a8c851a224e43e965541c97056
Tzu Chao <chao.tzu5@gmail.com>
2017-08-22 13:02:22 -0500

    redo focus logic

--- src/components/Focus/FocusContainer.js
+++ src/components/Focus/FocusContainer.js
@@ -1,18 +1,15 @@
-import React, {Component} from 'react';
-import AddForm from '../Todo/components/AddForm';
-import Item from '../Todo/components/Item';
 import * as FocusActionCreators from '../../actions/FocusActionCreators'
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import Focus from './Focus'
 //import {loadFromStorage, saveToStorage} from '../../localStorage';
-import './Focus.scss';
+
 const mapStateToProps=(state)=>{
   return {focus:state.focus};
 }
 const mapDispatchToProps=(dispatch)=>{
   return bindActionCreators(FocusActionCreators,dispatch);
 }
-class FocusContainer extends Component{
 /*  constructor(props){
     super(props);
     this.state=loadFromStorage('focus')||{
@@ -33,23 +30,5 @@ class FocusContainer extends Component{
   componentDidUpdate(){
     saveToStorage('focus',this.state);
   }*/
-  render(){
-    const {focus, setFocus, deleteFocus, toggleFocus}=this.props;
-    const display=focus.set?(
-      <Item className='FocusItem' item={focus} toggleItem={toggleFocus} deleteItem={deleteFocus} />
-    ):(
-      <div className='Focus'>
-        <span>What is your main focus for today?</span>
-      </div>
-    );
-    return(
-      <div>
-        {display}
-        {
-          focus.set?null:(<AddForm className='AddFocus' placeholder='' submit={setFocus}/>)
-        }
-      </div>
-    )
-  }
-}
-export default connect(mapStateToProps,mapDispatchToProps)(FocusContainer);
+const FocusContainer=connect(mapStateToProps,mapDispatchToProps)(Focus);
+export default FocusContainer;
