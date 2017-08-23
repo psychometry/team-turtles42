82cf884b117116f3a6a672d52e4ab2ce6d460ba1
Tzu Chao <chao.tzu5@gmail.com>
2017-08-22 19:34:26 -0500

    change app.test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+//import {Provider} from 'react-redux';
+//import {createStore} from 'redux';
 import App from './App';
 
 it('renders without crashing', () => {
-  const mockDefaultState={
+  /*const mockDefaultState={
     app:{todo:false},
     focus:{text:null,
       done:false,
@@ -19,7 +19,7 @@ it('renders without crashing', () => {
   const mockReducer=(state)=>{
     return state;
   }
-  const mockStore=createStore(mockReducer, mockDefaultState);
+  const mockStore=createStore(mockReducer, mockDefaultState);*/
   const div = document.createElement('div');
-  ReactDOM.render(<Provider store={mockStore}><App /></Provider>, div);
+  ReactDOM.render(<App />, div);
 });
